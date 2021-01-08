543c9349d271a88e6027c27b9e1e60092082e446
JayMartMedia <jay@jaymartmedia.com>
2021-01-08 16:38:43 -0500

    added button and patient table to homepage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
+import PatientTable from './components/PatientTable';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  })
+
+  test('renders title', () => {
+    const h1 = wrapper.find('h1');
+    expect(h1.text()).toContain('Demo Patient Tracker');
+  });
+
+  test('renders button to display table', () => {
+    const button = wrapper.find('button');
+    expect(button.text()).toContain('View Patients');
+  })
+
+  test('table is hidden on initial render', () => {
+    const patientTable = wrapper.find(PatientTable);
+    expect(patientTable).toHaveLength(0);
+  })
+
+  test('renders patient table after view patients is clicked', () => {
+    const button = wrapper.find('button');
+    button.simulate('click');
+
+    const patientTable = wrapper.find(PatientTable);
+    expect(patientTable).toHaveLength(1);
+  })
+
+  test('hides view patients button after it is clicked', () => {
+    const button = wrapper.find('button');
+    button.simulate('click');
+
+    expect(wrapper.find('button')).toHaveLength(0);
+  })
 });
---
+++ src/components/PatientTable.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PatientTable() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PatientTable
