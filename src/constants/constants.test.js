aba10e5aae5bd2de0850d1c814585ef19089173f
JayMartMedia <jay@jaymartmedia.com>
2021-01-11 01:12:32 -0500

    added patientConstants and test file

---
+++ src/constants/constants.test.js
@@ -0,0 +1,5 @@
+describe('constants', () => {
+    it('loads the patientConstants file', () => {
+        require('./patientConstants');
+    })
+})
---
+++ src/constants/patientConstants.js
@@ -0,0 +1,8 @@
+const PATIENT_CONSTANTS = {
+    TYPE: 'patient',
+    FIRST_NAME: 'firstName',
+    LAST_NAME: 'lastName',
+    DATE_OF_BIRTH: 'dateOfBirth'
+};
+
+export default PATIENT_CONSTANTS;
