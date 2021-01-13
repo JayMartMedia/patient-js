2647aafdcdf08eef4f27d6d1c34e8ef0de8c2e15
JayMartMedia <jay@jaymartmedia.com>
2021-01-13 01:09:19 -0500

    Added sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import {useState} from 'react';
-
 import PatientTableContainer from './components/table/PatientTableContainer';
 import Button from './components/buttons/Button';
-
 import {Rest} from './utility/rest';
+
+import './App.scss';
 
 function App() {
   const [viewPatients, setViewPatients] = useState(false);
@@ -15,7 +15,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header">
         <h1>Demo Patient Tracker</h1>
       </header>
